75a53c1269e1bb0ca08ee81849b47f84233d5e15
Kurucsai György <kurucsai.gyorgy@gmail.com>
2024-04-06 12:44:34 +0200

    Init commit - committing all files to the repository

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace Tutorial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.Clear();
+            //Console.WriteLine("Hello, Papa");
+            //Console.Write("What is your name? ");
+            ////Console.ForegroundColor = ConsoleColor.Black;
+            ////Console.BackgroundColor = ConsoleColor.White;
+
+            //string? name = Console.ReadLine();
+            //Console.WriteLine($"Hello {name}");
+
+            //Console.WriteLine("The biggest integer is: {0}", int.MaxValue);
+            //Console.WriteLine("The smallest integer is: {0}", int.MinValue);
+
+            //Console.WriteLine("The biggest long is: {0}", long.MaxValue);
+            //Console.WriteLine("The smallest long is: {0}", long.MinValue);
+
+
+            //decimal decPiVal = 3.144355465466788713246878978978779777m;
+            //decimal decBigNum = 3.00000000000000000000000000000000011m;
+
+            //Console.WriteLine("Dec : PI + BigNum = {0}", decPiVal + decBigNum);
+
+            //Console.WriteLine("The biggest decimal is: {0}", decimal.MaxValue);
+
+            //Console.WriteLine("The biggest double is: {0}", double.MaxValue);
+            //Console.WriteLine("The smallest double is: {0}", double.MinValue);
+
+            //bool boolFromStr = bool.Parse("true");
+            //Console.WriteLine(boolFromStr);
+
+            //int intFromStr = int.Parse("1000");
+            //Console.WriteLine(intFromStr);
+
+            //double dblFromStr = double.Parse("1.234");
+            //Console.WriteLine(dblFromStr);
+
+            //string strVal = dblFromStr.ToString();
+            //Console.WriteLine(strVal);
+
+            //Console.WriteLine($"Data type : {strVal.GetType()}");
+            //double dblNum = 12.345;
+            //Console.WriteLine($"Integer : {(int)dblNum}");
+
+            //int intNum = 10;
+            //long longNum = (long)intNum;
+
+            //Console.WriteLine("Currency : {0:c}", 23.455);
+            //Console.WriteLine("Pad with zeroes : {0:d4}", 23);
+            //Console.WriteLine("3 decimals : {0:f3}", 23.455456898987);
+            //Console.WriteLine("Commas : {0:n4}", 2300);
+
+            //string randStr = "This is a string.";
+            //Console.WriteLine("String length : {0}", randStr.Length);
+            //Console.WriteLine("String contains 'is' : {0}", randStr.Contains("is"));
+            //Console.WriteLine("Index of 'is' : {0}", randStr.IndexOf("is"));
+            //Console.WriteLine("Remove string : {0}", randStr.Remove(10, 7));
+            //Console.WriteLine("Insert string : {0}", randStr.Insert(10, "short "));
+            //Console.WriteLine("Replace string : {0}", randStr.Replace("string", "sentence"));
+            //Console.WriteLine("Compare A to B : {0}",
+            //    string.Compare("b", "B", StringComparison.OrdinalIgnoreCase));
+            //Console.WriteLine("----------------------------------------");
+            //Console.WriteLine("Pad Left : {0}", randStr.PadLeft(30, '.'));
+            //Console.WriteLine("Pad Right: {0}", randStr.PadRight(30, '.'));
+            //Console.WriteLine("Trim: {0}", randStr.Trim());
+            //Console.WriteLine("Uppercase: {0}", randStr.ToUpper());
+            //Console.WriteLine("Uppercase: {0}", randStr.ToLower());
+
+            //string newString = String.Format("{0} saw a {1} {2} in the {3}.", "Paul", "rabbit", "eating", "field");
+            //Console.WriteLine(newString);
+            //Console.Write(newString + '\n');
+            //// \' \" \\ \t \a
+            //Console.WriteLine(@"Exactly what I typed\n");
+
+            // Arrays 
+
+
+
+        }
+    }
+}
+
